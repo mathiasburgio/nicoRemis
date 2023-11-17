44df646fc7a90c81c424178c5b873823a0fd5813
Mathias Burgio <mathias.b@live.com.ar>
2023-11-17 08:07:17 -0300

    agregado el filtro por dia de mes en resumen

--- src/scripts/Resumen.js
+++ src/scripts/Resumen.js
@@ -17,11 +17,24 @@ class Resumen{
             if(!v) return;
             const anio = v.split("-")[0];
             const mes = v.split("-")[1];
+
+            let dias = fechas.days_in_month(anio, mes);
+            let optDias = `<option value='0' selected>Mes completo</option>`;
+            for(let i = 1; i <= dias; i++){
+                optDias += `<option value='${i}'>Día ${i}</option>`;
+            }
+            $("[name='dia']").html(optDias);
+
             this.viajes = (await $.get({ url: "/viajes/get-month/" + anio + "/" + mes })).result;
             this.registrosCajas =  (await $.get({ url: "/cajas/get-month/" + anio + "/" + mes })).result;
             console.log(this.viajes, this.registrosCajas);
             this.listarMes();
         }).change()
+
+
+        $("[name='dia']").change(ev=>{
+            this.listarMes();
+        });
 
         $("[name='imprimir']").click(async ev=>{
             let response = await $.post({
@@ -42,6 +55,7 @@ class Resumen{
     }
     listarMes(){
         const fecha = $("[name='mes']").val();
+        const dia = Number( $("[name='dia']").val() );
         const anio = fecha.split("-")[0];
         const mes = fecha.split("-")[1];
 
@@ -59,20 +73,28 @@ class Resumen{
             </div>`;
         }
         
-        
+        let previaje = JSON.parse(JSON.stringify(this.viajes));
+        if(dia != 0){
+            previaje = previaje.filter(v=>{
+                let ofx = fechas.parse({val: v.fechaPartida});
+                return (Number(ofx.dia) === dia);
+            })
+        }
+
+
         let html = "";
         html += "<div class='text-center h3'>" + fechas.MONTH_NAME[Number(mes) -1] + " de " + anio + "</div>";
         html += `<small>Impreso el ${fechas.parse2(new Date(), "ARG_FECHA_HORA")}</small>`;
         html += titulo("Viajes TOTALES");
-        html += linea1("Agendados: ", this.viajes.length);
-        html += linea1("Concretados: ", this.viajes.filter(v=>v.estado == 3).length);
-        html += linea1("Cancelados: ", this.viajes.filter(v=>v.estado == 4).length);
-        html += linea1("Km. recorridos: ", this.viajes.reduce((acc, v)=> acc + (v.estado == 3 ? v.kilometrosRecorrer : 0), 0));
-        html += linea1("Ingresos brutos (cobrados): ", this.viajes.reduce((acc, v)=> acc + (v.cobrado ? v.valorViaje : 0), 0));
+        html += linea1("Agendados: ", previaje.length);
+        html += linea1("Concretados: ", previaje.filter(v=>v.estado == 3).length);
+        html += linea1("Cancelados: ", previaje.filter(v=>v.estado == 4).length);
+        html += linea1("Km. recorridos: ", previaje.reduce((acc, v)=> acc + (v.estado == 3 ? v.kilometrosRecorrer : 0), 0));
+        html += linea1("Ingresos brutos (cobrados): ", previaje.reduce((acc, v)=> acc + (v.cobrado ? v.valorViaje : 0), 0));
 
         html += titulo("Transportes");
         this.transportes.forEach(tx=>{
-            let _viajes = this.viajes.filter(v=>v.transporte == tx._id);
+            let _viajes = previaje.filter(v=>v.transporte == tx._id);
             if(_viajes.length > 0){
                 html += titulo2(tx.nombre);
                 html += linea1("Agendados: ", _viajes.length);
@@ -85,7 +107,7 @@ class Resumen{
 
         html += titulo("Choferes");
         this.choferes.forEach(tx=>{
-            let _viajes = this.viajes.filter(v=>v.chofer == tx._id);
+            let _viajes = previaje.filter(v=>v.chofer == tx._id);
             if(_viajes.length > 0){
                 html += titulo2(tx.nombre);
                 html += linea1("Agendados: ", _viajes.length);
